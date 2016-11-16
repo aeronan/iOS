8602c45b94034409b5ff1c97e124f86dfafba8fd
Aeron An <btyegj@gmail.com>
2016-11-16 18:09:13 +1000

    add function

--- WhatsTheNumber/WhatsTheNumber/ViewController.swift
+++ WhatsTheNumber/WhatsTheNumber/ViewController.swift
@@ -13,27 +13,39 @@ class ViewController: UIViewController {
     var answer = Int(arc4random_uniform(100) + 1)
     var maxNumber = 100
     var minNumber = 1
+    var isOver = false
+    
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var inputTextField: UITextField!
     
     @IBAction func makeAGuess(sender: UIButton) {
-        let inputNumber = Int(inputTextField.text!)
-        inputTextField.text = ""
-        
-        if inputNumber > maxNumber{
-            messageLabel.text = "Too large! Guess a number between\(minNumber)~\(maxNumber)"
-        }else if inputNumber < minNumber{
-           messageLabel.text = "Too small! Guess a number between\(minNumber)~\(maxNumber)"
-        }else if inputNumber == answer{
-            messageLabel.text = "You are right"
-        }else{
-            if inputNumber > answer{
-                maxNumber = inputNumber!
-                messageLabel.text = "Guess a number between\(minNumber)~\(maxNumber)"
+        if isOver == false{
+            let inputNumber = Int(inputTextField.text!)
+            inputTextField.text = ""
+            
+            if inputNumber > maxNumber{
+                messageLabel.text = "Too large! Guess a number between\(minNumber)~\(maxNumber)"
+            }else if inputNumber < minNumber{
+               messageLabel.text = "Too small! Guess a number between\(minNumber)~\(maxNumber)"
+            }else if inputNumber == answer{
+                messageLabel.text = "You are right! Press [Guess] to play again!"
+                isOver = true
             }else{
-                minNumber = inputNumber!
-                messageLabel.text = "Guess a number between\(minNumber)~\(maxNumber)"
+                if inputNumber > answer{
+                    maxNumber = inputNumber!
+                    messageLabel.text = "Guess a number between\(minNumber)~\(maxNumber)"
+                }else{
+                    minNumber = inputNumber!
+                    messageLabel.text = "Guess a number between\(minNumber)~\(maxNumber)"
+                }
             }
+        }else{
+            maxNumber = 100
+            minNumber = 1
+            messageLabel.text = "Guess a number between\(minNumber)~\(maxNumber)"
+            answer = Int(arc4random_uniform(100) + 1)
+            print(answer)
+            isOver = false
         }
     }
     
